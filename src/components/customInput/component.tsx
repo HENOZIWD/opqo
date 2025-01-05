86197fa1e1e6a922b430206da28905ed6453f97b
Donghyeon Lee <ehdgus001928@gmail.com>
2025-01-05 23:08:37 +0900

    feat: CustomInput 컴포넌트 label 추가

--- src/components/customInput/component.tsx
+++ src/components/customInput/component.tsx
@@ -3,14 +3,26 @@ import styles from './style.module.css';
 import { FieldValues, Path, UseFormRegister } from 'react-hook-form';
 
 interface CustomInputProps<T extends FieldValues> extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
   name: Path<T>;
   register: UseFormRegister<T>;
 }
 
-export default function CustomInput<T extends FieldValues>({ name, register, ...props }: CustomInputProps<T>) {
+export default function CustomInput<T extends FieldValues>({ label, name, register, ...props }: CustomInputProps<T>) {
   return (
     <div className={styles.container}>
-      <input className={styles.input} {...props} {...register(name)} />
+      <label
+        htmlFor={name}
+        className={styles.label}
+      >
+        {label}
+      </label>
+      <input
+        className={styles.input}
+        id={name}
+        {...props}
+        {...register(name)}
+      />
     </div>
   );
 }
