1a368e9cc7f9d3a61ee6afc15359fb557ecc8149
Donghyeon Lee <ehdgus001928@gmail.com>
2025-01-13 03:37:00 +0900

    feat: 비동기 처리 핸들링 함수 구현

---
+++ src/utils/fetchHandler.ts
@@ -0,0 +1,17 @@
+import { AxiosResponse } from 'axios';
+
+export const fetchHandler = async <T>(
+  fetchFn: () => Promise<AxiosResponse<T>>,
+  { onSuccess, onError }: {
+    onSuccess: (response?: AxiosResponse<T>) => void;
+    onError: (error?: unknown) => void;
+  },
+) => {
+  try {
+    const response = await fetchFn();
+    onSuccess(response);
+  }
+  catch (error) {
+    onError(error);
+  }
+};
