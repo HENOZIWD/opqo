2d1453143e6c7b3234a84313dca1632eaf824bb2
Donghyeon Lee <ehdgus001928@gmail.com>
2025-02-02 22:56:01 +0900

    feat: 채널 프로필 이미지 컴포넌트 수정
    - 오류 메시지 추가
    - input 선택 허용 파일 제한

--- src/components/channelImageSelector/component.tsx
+++ src/components/channelImageSelector/component.tsx
@@ -2,25 +2,31 @@ import Image from 'next/image';
 import styles from './style.module.css';
 import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
 import { isValidImageSize, isValidImageType } from '@/utils/image';
+import { ERR_MSG_FILE_LOAD_ERROR, ERR_MSG_INVALID_IMAGE_SIZE, ERR_MSG_INVALID_IMAGE_TYPE } from '@/utils/message';
 
 interface ChannelImageSelectorProps { setImageData: Dispatch<SetStateAction<Blob | null>> }
 
 export default function ChannelImageSelector({ setImageData }: ChannelImageSelectorProps) {
   const [imagePreviewUrl, setImagePreviewUrl] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
   const handleUploadImage = (e: ChangeEvent<HTMLInputElement>) => {
+    setErrorMessage('');
     const fileList = e.target.files;
 
     if (!fileList) {
+      setErrorMessage(ERR_MSG_FILE_LOAD_ERROR);
       return;
     }
 
     if (fileList.length > 0) {
       if (!isValidImageType(fileList[0].type)) {
+        setErrorMessage(ERR_MSG_INVALID_IMAGE_TYPE);
         return;
       }
 
       if (!isValidImageSize(fileList[0].size)) {
+        setErrorMessage(ERR_MSG_INVALID_IMAGE_SIZE);
         return;
       }
 
@@ -33,26 +39,30 @@ export default function ChannelImageSelector({ setImageData }: ChannelImageSelec
 
   return (
     <div className={styles.container}>
-      <div className={styles.previewWrapper}>
-        <Image
-          className={styles.preview}
-          src={imagePreviewUrl || '/assets/lightgray.png'}
-          alt="프로필 이미지 미리보기"
-          fill
+      <div className={styles.inputWrapper}>
+        <div className={styles.previewWrapper}>
+          <Image
+            className={styles.preview}
+            src={imagePreviewUrl || '/assets/lightgray.png'}
+            alt="프로필 이미지 미리보기"
+            fill
+          />
+        </div>
+        <input
+          type="file"
+          id="channelImageSelector"
+          className={styles.input}
+          onChange={handleUploadImage}
+          accept="image/jpeg,image/png,image/webp"
         />
+        <label
+          htmlFor="channelImageSelector"
+          className={styles.label}
+        >
+          채널 프로필 이미지 선택
+        </label>
       </div>
-      <input
-        type="file"
-        id="channelImageSelector"
-        className={styles.input}
-        onChange={handleUploadImage}
-      />
-      <label
-        htmlFor="channelImageSelector"
-        className={styles.label}
-      >
-        채널 프로필 이미지 선택
-      </label>
+      {errorMessage && <div className={styles.error}>{errorMessage}</div>}
     </div>
   );
 }
--- src/utils/message.ts
+++ src/utils/message.ts
@@ -18,6 +18,8 @@ export const ERR_MSG_VALIDATION_TIME_EXPIRED = '인증 제한 시간이 만료�
 export const ERR_MSG_INVALID_USER = '일치하는 계정 정보가 없습니다.';
 
 // createChannel
+export const ERR_MSG_INVALID_IMAGE_TYPE = '이미지 파일은 .jpeg, .png, .webp 확장자만 업로드 가능합니다.';
+export const ERR_MSG_INVALID_IMAGE_SIZE = '업로드 가능한 이미지 파일 크기는 최대 5 MB입니다.';
 export const ERR_MSG_CHANNELNAME_RULE = '채널명은 2-20자 한글, 영문, 숫자, 공백, 밑줄(_), 대시(-)로 구성되어야 합니다. 공백으로 시작하거나 끝날 수 없으며, 연속된 공백은 허용되지 않습니다.';
 export const ERR_MSG_DUPLICATED_CHANNELNAME = '이미 존재하는 채널명입니다.';
 
