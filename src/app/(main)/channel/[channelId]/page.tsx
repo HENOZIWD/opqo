8d22ba6a853f9d356435aab3330c43e69a18537c
Donghyeon Lee <ehdgus001928@gmail.com>
2025-02-18 23:15:35 +0900

    feat: 채널 페이지 수정
    - 불필요한 API 요청 방지

--- src/app/(main)/channel/[channelId]/page.tsx
+++ src/app/(main)/channel/[channelId]/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 
 import styles from './page.module.css';
-import useSWR from 'swr';
 import { channelGETFetcher } from '@/apis/channel';
 import { useParams } from 'next/navigation';
 import ChannelProfile from '@/components/channelProfile/component';
 import VideoCard from '@/components/videoCard/component';
+import useSWRImmutable from 'swr/immutable';
 
 export default function ChannelPage() {
   const { channelId } = useParams<{ channelId: string }>();
 
-  const { data: channelData } = useSWR(`/channel/${channelId}`, channelGETFetcher);
-  const { data: videoListData } = useSWR(`/channel/${channelId}/videos`, channelGETFetcher);
+  const { data: channelData } = useSWRImmutable(`/channel/${channelId}`, channelGETFetcher);
+  const { data: videoListData } = useSWRImmutable(`/channel/${channelId}/videos`, channelGETFetcher);
 
   return (
     <main>
@@ -25,31 +25,33 @@ export default function ChannelPage() {
           />
         )
         : null}
-      <h2 className={styles.section}>업로드한 동영상</h2>
-      <ul className={styles.videoList}>
-        {videoListData
-          ? videoListData.data.map(({
-            videoId,
-            thumbnailUrl,
-            videoDuration,
-            videoTitle,
-            uploadDate,
-          }) => (
-            <li
-              className={styles.videoCard}
-              key={videoId}
-            >
-              <VideoCard
-                videoId={videoId}
-                thumbnailUrl={thumbnailUrl}
-                videoTitle={videoTitle}
-                videoDuration={videoDuration}
-                uploadDate={uploadDate}
-              />
-            </li>
-          ))
-          : null}
-      </ul>
+      <section>
+        <h2 className={styles.sectionTitle}>업로드한 동영상</h2>
+        <ul className={styles.videoList}>
+          {videoListData
+            ? videoListData.data.map(({
+              videoId,
+              thumbnailUrl,
+              videoDuration,
+              videoTitle,
+              uploadDate,
+            }) => (
+              <li
+                className={styles.videoCard}
+                key={videoId}
+              >
+                <VideoCard
+                  videoId={videoId}
+                  thumbnailUrl={thumbnailUrl}
+                  videoTitle={videoTitle}
+                  videoDuration={videoDuration}
+                  uploadDate={uploadDate}
+                />
+              </li>
+            ))
+            : null}
+        </ul>
+      </section>
     </main>
   );
 }
