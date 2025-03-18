eefc5d99cb62ed7020b5877bace2781951094906
henoziwd <ehdgus001928@gmail.com>
2025-03-18 14:26:01 +0900

    feat: numberToTime 함수 수정
    - NaN 검사 로직 추가

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,4 +1,8 @@
 export function numberToTime(second: number) {
+  if (Number.isNaN(Number(second))) {
+    return null;
+  }
+
   if (second > 3600) {
     return `${(second / 3600) >> 0}:${(((second % 3600) / 60) >> 0).toString().padStart(2, '0')}:${((second % 60) >> 0).toString().padStart(2, '0')}`;
   }
