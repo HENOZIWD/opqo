d4f2fef1e43cb7bbacd36e75bc266896a6098f20
Donghyeon Lee <ehdgus001928@gmail.com>
2025-03-23 15:15:28 +0900

    feat: metadata title 적용

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,7 @@
 import Header from '@/components/header/component';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = { title: 'OpqO' };
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
