d278b24fd03a8f4176f60960747ca74e0dc28526
Donghyeon Lee <ehdgus001928@gmail.com>
2025-04-15 19:22:50 +0900

    feat: Static 페이지 title 추가

--- src/app/(auth)/createChannel/page.tsx
+++ src/app/(auth)/createChannel/page.tsx
@@ -1,5 +1,8 @@
+import { Metadata } from 'next';
 import styles from './page.module.css';
 import CreateChannelForm from '@/components/createChannelForm/component';
+
+export const metadata: Metadata = { title: '채널 생성' };
 
 export default function CreateChannelPage() {
   return (
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,4 @@
 import Header from '@/components/header/component';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = { title: 'OpqO' };
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
