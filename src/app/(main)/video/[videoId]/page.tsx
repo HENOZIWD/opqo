dd7e8d90dfa3e3bc83821b5e89577b5e31b77e41
Donghyeon Lee <ehdgus001928@gmail.com>
2025-04-15 22:33:05 +0900

    feat: Dynamic 페이지 Metadata 추가
    - title, description, openGraph images

--- src/app/(main)/video/[videoId]/page.tsx
+++ src/app/(main)/video/[videoId]/page.tsx
@@ -1,6 +1,23 @@
+import { getVideoInfo } from '@/apis/video';
 import VideoFetcher from '@/fetcher/videoFetcher/component';
+import { fetchHandlerWithServerComponent } from '@/utils/handler';
+import { Metadata } from 'next';
 
-export default async function VideoPage({ params }: { params: Promise<{ videoId: string }> }) {
+interface VideoPageProps { params: Promise<{ videoId: string }> }
+
+export async function generateMetadata({ params }: VideoPageProps): Promise<Metadata | void> {
+  const { videoId } = await params;
+
+  const { data } = await fetchHandlerWithServerComponent(() => getVideoInfo({ videoId }));
+
+  return {
+    title: `${data?.title}`,
+    description: `${data?.description}`,
+    openGraph: { images: [`${process.env.NEXT_PUBLIC_CDN_THUMBNAIL_URL}/${data?.id}`] },
+  };
+}
+
+export default async function VideoPage({ params }: VideoPageProps) {
   const { videoId } = await params;
 
   return (
