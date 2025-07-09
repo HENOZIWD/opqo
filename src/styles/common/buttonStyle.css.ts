54ab13255b52d34bc5cdd7d235ac1998f6f09426
Donghyeon Lee <ehdgus001928@gmail.com>
2025-07-09 18:43:29 +0900

    fix: 버튼 스타일 정상적으로 적용되지 않는 문제 수정

--- src/styles/common/buttonStyle.css.ts
+++ src/styles/common/buttonStyle.css.ts
@@ -1,32 +1,30 @@
 import { style } from '@vanilla-extract/css';
 import { colorStyleVars } from '../common.css';
 
+const buttonDefaultStyle = style({
+  'display': 'flex',
+  'borderRadius': 9999,
+  'background': colorStyleVars.black,
+  'color': colorStyleVars.white,
+  'cursor': 'pointer',
+  'whiteSpace': 'nowrap',
+  'padding': '0.625rem 1.25rem',
+  'fontWeight': 900,
+  'fontSize': '1.125rem',
+  'justifySelf': 'center',
+
+  ':disabled': {
+    background: colorStyleVars.lightGray,
+    cursor: 'default',
+  },
+});
+
 export const buttonStyle = {
-  default: style({
-    'display': 'flex',
-    'borderRadius': 9999,
-    'background': colorStyleVars.black,
-    'color': colorStyleVars.white,
-    'cursor': 'pointer',
-    'whiteSpace': 'nowrap',
-    'padding': '0.625rem 1.25rem',
-    'fontWeight': 900,
-    'fontSize': '1.125rem',
-    'justifySelf': 'center',
+  default: buttonDefaultStyle,
 
-    ':disabled': {
-      background: colorStyleVars.lightGray,
-      cursor: 'default',
-    },
-
-    ':focus': { boxShadow: `0 0 0 0.25rem ${colorStyleVars.blackShadow}` },
-
-    ':hover': { boxShadow: `0 0 0 0.25rem ${colorStyleVars.blackShadow}` },
-  }),
-
-  small: style({
+  small: style([buttonDefaultStyle, {
     padding: '0.375rem 0.875rem',
     fontWeight: 700,
     fontSize: '0.875rem',
-  }),
+  }]),
 };
