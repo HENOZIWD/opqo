3f72eac0567f5fe3c19dc639e1f58d594d504d03
Donghyeon Lee <ehdgus001928@gmail.com>
2025-07-31 23:05:47 +0900

    feat: api 요청 인스턴스 기본 timeout 30초로 변경

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -3,6 +3,6 @@ import ky from 'ky';
 
 export const fetchInstance = ky.create({
   prefixUrl: process.env.NEXT_PUBLIC_SERVER_URL,
-  timeout: 10000,
+  timeout: 30000,
   hooks: { afterResponse: [tokenRefreshInterceptor] },
 });
